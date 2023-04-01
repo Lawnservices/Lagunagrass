53da814a1785144992b2f02cc1d5bf9d951be69c
miguel <80590513+Lawnservices@users.noreply.github.com>
2023-04-01 18:38:03 -0400

    fix

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Miguel Lawn 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div class="header">
+          <ul>
+            <li><a href="https://lagunalawnservice.com/">Home</a></li>
+            <li><a href="https://lagunalawnservice.com/prices">Prices</a></li>
+            <div class="down">
+              <a href='.' id="vermas">More</a>
+              <div class="down-content">
+                <a href="https://www.google.com/maps/place/Miguel+Lawn+Services/@38.9627497,-76.8575863,17z/data=!3m1!4b1!4m5!3m4!1s0x8f6d334b1188e79f:0x977b2db02a46cf85!8m2!3d38.9627497!4d-76.8575863">Address</a>
+                <a href="https://www.youtube.com/channel/UCaZ_FoEDUrrHj_FKiEPLkyA/featured" title="Subscribe to the channel">Youtube</a>
+                <a href="https://cash.app/account/activity">CashApp</a>
+                <a href="https://wa.link/7f91om">WhatsApp</a>
+
+              </div>
+            </div>
+          </ul>
+        </div>
       </header>
     </div>
   );
